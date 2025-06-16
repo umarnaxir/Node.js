8014a79050782b023840f8cc8db17ab0ba6617af
umarnaxir <erumarnazir@gmail.com>
2025-06-16 23:53:46 +0530

    added

---
+++ 05_colors.js
@@ -0,0 +1,5 @@
+const colors = require("colors");
+
+console.log(colors.red("Hi, this is Umar Nazir"));
+console.log(colors.green("Hi, this is Umar Nazir"));
+console.log(colors.yellow("Hi, this is Umar Nazir"));
